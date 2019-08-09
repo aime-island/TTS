b58441cb82d6789de0932dfebb3d17034cbbdc35
Viktor Sveinsson <viktorsveins@gmail.com>
2019-08-09 15:44:05 +0000

    preprocess function for moz/TTS dataloader

---
+++ preprocess_sunneva.py
@@ -0,0 +1,13 @@
+import os
+
+# Preprocess function so Sunneva can be used with TTS's generic dataloader
+def sunneva(root_path, meta_file):
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    with open(txt_file, 'r') as ttf:
+        for line in ttf:
+            cols = line.split("\t")
+            text = cols[0]
+            wav_file = os.path.join(root_path, "audio", cols[1] + ".wav")
+            items.append([text, wav_file])
+    return items
