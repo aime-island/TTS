328e30f5f02db9408efbd4ec15291759fe03f370
Viktor Sveinsson <viktorsveins@gmail.com>
2019-08-09 17:01:34 +0000

    added speaker name

--- preprocess_sunneva.py
+++ preprocess_sunneva.py
@@ -4,10 +4,11 @@ import os
 def sunneva(root_path, meta_file):
     txt_file = os.path.join(root_path, meta_file)
     items = []
+    speaker_name = 'sunneva'
     with open(txt_file, 'r') as ttf:
         for line in ttf:
             cols = line.split("\t")
             text = cols[0]
             wav_file = os.path.join(root_path, "audio", cols[1] + ".wav")
-            items.append([text, wav_file])
+            items.append([text, wav_file, speaker_name])
     return items
